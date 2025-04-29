f9bbe346eaea52487fb071ffcbd4c02a1dc2d686
Andres Alban <bethesdas101@gmail.com>
2025-04-29 15:39:32 -0500

    fixing typo

--- internal/models/volume.go
+++ internal/models/volume.go
@@ -3,7 +3,7 @@ package models
 // VolumeResponse API response struct.
 type VolumeResponse struct {
 	SpecificVolumeLiquid float64 `json:"specific_volume_liquid"`
-	SpecificVolumeVapor  float64 `json:"specific_volumne_vapor"`
+	SpecificVolumeVapor  float64 `json:"specific_volume_vapor"`
 }
 
 // NewVolumeResponse returns an instance of VolumeResponse.
